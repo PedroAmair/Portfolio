da30358356473ffe23d22efb1dbc8c9c5a4d9dd4
Pedro Amair <pedroamair@hotmail.com>
2024-02-23 15:44:51 -0400

    agregado normaize.css

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,7 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         @stack('styles')
-        @vite(['resources/css/app.css','resources/css/styles.css','resources/js/animeEffects.js','resources/js/app.js'])
+        @vite(['resources/css/app.css','resources/css/styles.css',
+               'resources/js/animeEffects.js','resources/js/app.js',
+               'resources/css/normalize.css'])
         @livewireStyles
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
