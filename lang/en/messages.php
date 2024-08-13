a800736bf4355878ef1ae2aab1e57f36066b7098
Pedro Amair <pedroamair@hotmail.com>
2024-08-13 16:07:28 -0400

    mejoras en la redacción de los detalles de cada proyecto

--- lang/en/messages.php
+++ lang/en/messages.php
@@ -17,26 +17,25 @@ return [
      without using any framework.",
     'project1Content2' => "The web portal has an administrative 
      section divided into two areas with their respective CRUD's 
-     for the management of both administrative users and 
+     for the management of both, administrative users and 
      published vehicles.",
     'project1Content3' => "For access to the administrative section 
      of the project you can contact me to pass you the credentials 
      of a test user.",
-    'project2Content1' => "Web system type project about reservations
-     in a party club. It was developed using: HTML5, CSS3, JavaScript,
+    'project2Content1' => "Web project type system for making reservations
+     at a party club. It was developed using: HTML5, CSS3, JavaScript,
      PHP, Gulp, Sass, MVC, POO, Fetch API.",
     'project2Content2' => "In the customer section you can make reservations
      at the club and choose from the various services available for any
      festive event. You can select the day and time of the booking. At the 
-     end, the user will have a detailed report on their reservation for them 
-     to verify. If confirmed, the reservation will be finalized and can be 
-     viewed in the administrative section.",
+     end, the user will have a detailed report on his reservation for verification. 
+     If the user agrees, the reservation will be confirmed and can be viewed in the 
+     administrative section.",
     'project2Content3' => "Everything associated with the booking was done using
      Rest API.",
-    'project2Content4' => "In the administrative section you can read all customer
-     reservations and search for them by date as well. In this area, new services 
-     can be created so that customers have more options when customizing their 
-     reservation and those services can also be modified.",
+    'project2Content4' => "In the administrative section you can view all customer reservations 
+     and consult them by date. In this area you can create new services so that customers have 
+     more options when customizing their reservation. Likewise, these services can also be modified.",
     'project3Content1' => "Web project about a website similar to Amazon/MercadoLibre/Bestbuy. 
      Developed using Laravel framework and adding Livewire, Tailwind CSS, Flowbite and native 
      JavaScript. The website is still under development.",
