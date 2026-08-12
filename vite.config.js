c3332246ea87eaa2362e3ad11fb76678fd9fa9fa
Pedro Amair <pedroamair@hotmail.com>
2026-08-12 16:46:54 -0400

    Agregado archivo javascript para el paginador

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/css/styles.css',
-                    'resources/js/animeEffects.js', 'resources/js/app.js'],
+                    'resources/js/animeEffects.js', 'resources/js/app.js',
+                    'resources/js/paginadorSinMover.js'],
             refresh: true,
         }),
     ],
